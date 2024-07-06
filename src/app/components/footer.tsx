704b7a90947e80ed88c04e6f61f4e29a1774bbec
archey347 <archey.barrell@outlook.com>
2024-07-06 17:33:56 +0100

    Add sponsors to footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,32 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Footer() {
+    const sponsors = [
+        "Chris Hillman",
+        "Caldicot Windows & Building Services",
+        "Abergavenny Brake & Clutch",
+        "Foundry 4x4",
+        "Muddy Series 4X4 Parts",
+        "Whitecliff 4X4"
+    ];
+    
+    const [sponsorIndex, setSponsorIndex] = useState(Math.floor(Math.random() * sponsors.length))
+
+    setInterval(() => {
+        sponsorIndex < sponsors.length - 1 ? setSponsorIndex(sponsorIndex + 1) : setSponsorIndex(0)
+    }, 5000)
+
     return (
         <main>
-            <a href="https://wwlrc.co.uk/">&copy; {new Date().getFullYear()} Wye & Welsh Land Rover Club</a>
+            <p className="mb-2">
+                <Link href="/sponsors">Sponsored by {sponsors[sponsorIndex]}</Link>
+            </p>
+            <p>
+                <a href="https://wwlrc.co.uk/">&copy; {new Date().getFullYear()} Wye & Welsh Land Rover Club</a>
+            </p>
+            
         </main>
     )
 }
