111cbaedaa80b513556c36cb3d8175a08edd5840
archey347 <archey.barrell@outlook.com>
2024-07-06 02:38:06 +0100

    Make mobile navbar work

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import Link from "next/link";
 import NavbarBtn from "./navbar_btn";
+import React from "react";
 
 export default function Navbar() {
+
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+    function navbarToggle() {
+        setNavbarOpen(!navbarOpen);
+    }
+
     return (
         <nav>
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
@@ -12,13 +22,13 @@ export default function Navbar() {
                         Land Rover Club
                     </span>
                 </Link>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button onClick={navbarToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <div hidden={!navbarOpen} className="w-full md:block md:w-auto" id="navbar-default">
                 <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                     <li>
                         <Link href="/">
