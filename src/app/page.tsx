78efce4f95750e90409092c89cbf4d1d7176a53a
Archey Barrell <archey.barrell@outlook.com>
2024-07-07 13:55:23 +0100

    Add Challenge info
    First example of a web edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,11 @@ export default function Home() {
     <main>
       <h1 className="text-2xl font-bold mb-3">Home</h1>
 
-      <p>Welcome to the Wye & Welsh Land Rover Club website!</p>
+      <p className="mb-2">Welcome to the Wye & Welsh Land Rover Club website!</p>
+
+      <h2 className="text-xl">Wye & Welsh Challenge</h2>
+      <p className="mb-2">Bookings for the Wye & Welsh Challenge are now open! Visit the link below for more information, and to book:</p>
+      <a className="mb-2 underline text-blue" href="https://spanner.wwlrc.co.uk/rallies/r/22">https://spanner.wwlrc.co.uk/rallies/r/22</a>
     </main>
   );
 }
