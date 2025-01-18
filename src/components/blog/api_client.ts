c2c01c8c01480cbdc95da6da5bc816a75381987b
archey347 <archey.barrell@outlook.com>
2025-01-18 23:29:26 +0000

    Fix post ordering, and limit to 10

--- src/components/blog/api_client.ts
+++ src/components/blog/api_client.ts
@@ -1,3 +1,3 @@
 export function getPosts() {
-    return fetch('https://spanner.wwlrc.co.uk/api/clubs/c/2/blog_posts').then((res) => res.json()).then((res) => { return res.rows })
+    return fetch('https://spanner.wwlrc.co.uk/api/clubs/c/2/blog_posts?size=10&page=0&sorts[created_at]=desc').then((res) => res.json()).then((res) => { return res.rows })
 }
