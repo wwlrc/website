00b9e63b6a1d6d24d704427df9a0873868b372c4
archey347 <archey.barrell@outlook.com>
2025-01-19 15:11:47 +0000

    Make calendar pretty

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -2,6 +2,54 @@
 
 import { useState, useEffect, useCallback } from 'react'
 import { getEvents } from '@/spanner/events';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import { pathcat } from 'pathcat';
+
+dayjs.extend(advancedFormat)
+
+function renderDate(event: any) {
+    const f = (ds: string) => {
+        let d = dayjs(ds)
+
+        return d.format("ddd Do MMM YYYY")
+    }
+
+    if (event.is_one_day_event) {
+        return f(event.start_date)
+    }
+
+    return f(event.start_date) + " - " + f(event.end_date)
+}
+
+function renderStatus(event: any) {
+    let status = event.bookings_status;
+
+    if(status == "not_opened") {
+        return <a href={pathcat('https://spanner.wwlrc.co.uk', '/rallies/r/:id', event)} target="_blank">
+            <button className="px-2 py-2 bg-blue-600 text-white font-bold rounded-lg text-sm">Opening Soon</button>
+        </a>
+    }
+
+    if(status == "closed") {
+        return <a href={pathcat('https://spanner.wwlrc.co.uk', '/rallies/r/:id', event)} target="_blank">
+            <button className="px-2 py-2 bg-red-600 text-white font-bold rounded-lg text-sm">Bookings Closed</button>
+        </a>
+    }
+
+
+    if(status == "open") {
+        return <a href={pathcat('https://spanner.wwlrc.co.uk', '/rallies/r/:id', event)} target="_blank">
+            <button className="px-2 py-2 bg-green-600 text-white font-bold rounded-lg hover:bg-green-200">Book Now</button>
+        </a>
+    }
+
+    return
+}
+
+function renderLocation(location: any) {
+    return location.name + " (" + location.postcode + ")"
+}
 
 export default function SpannerCalendar({ staticEvents }: any) {
     const [events, setEvents] = useState(staticEvents)
@@ -24,9 +72,35 @@ export default function SpannerCalendar({ staticEvents }: any) {
 
     if(!events) return <p>No Events :/</p>
     return (<div>
-            {events.map((event: any, id: number) => (
-                <p key={id}>{event.name}</p>
-            ))}
+                <table className="table-auto rounded-lg border border-solid border-gray-200 dark:border-gray-600 w-full">
+                    <thead>
+                        <tr>
+                            <th className="px-4 py-2 border border-solid border-gray-200 dark:border-gray-600 hidden md:table-cell">Date</th>
+                            <th className="px-4 py-2 border border-solid border-gray-200 dark:border-gray-600">Event</th>
+                            <th className="px-4 py-2 border border-solid border-gray-200 dark:border-gray-600">Bookings</th>
+                            <th className="px-4 py-2 border border-solid border-gray-200 dark:border-gray-600 hidden md:table-cell">Location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {events.map((event: any, id: number) => {
+                            let d = new Date(event.end_date)
+
+                            if (d < (new Date())) {
+                                return
+                            }
+
+                            return <tr className="cursor-pointer hover:bg-gray-100 hover:dark:bg-gray-700" key={id} onClick={() => { window.open(pathcat('https://spanner.wwlrc.co.uk/', '/rallies/r/:id', event), '_blank')}}>
+                                <td className="px-2 py-1 md:px-4 md:py-2 border border-solid border-gray-200 dark:border-gray-600 hidden md:table-cell" >{renderDate(event)}</td>
+                                <td className="px-2 py-1 md:px-4 md:py-2 border border-solid border-gray-200 dark:border-gray-600">
+                                    <p className="md:hidden"><strong>{renderDate(event)}</strong></p>
+                                    <p>{event.name}</p>
+                                </td>
+                                <td className="px-2 py-1 md:px-4 md:py-2 border border-solid border-gray-200 dark:border-gray-600 text-center">{renderStatus(event)}</td>
+                                <td className="px-2 py-1 md:px-4 md:py-2 border border-solid border-gray-200 dark:border-gray-600 hidden md:table-cell">{renderLocation(event.location)}</td>
+                            </tr>
+})}
+                    </tbody>
+                </table>            
         </div>
     )
 }
