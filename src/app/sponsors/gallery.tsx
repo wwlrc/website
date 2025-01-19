00a88db4589beb9821cd1ea1fa4c2abce301bf80
archey347 <archey.barrell@outlook.com>
2025-01-19 22:09:35 +0000

    Fix sponsor page

--- src/app/sponsors/gallery.tsx
+++ src/app/sponsors/gallery.tsx
@@ -1,44 +1,79 @@
 "use client";
-import { Gallery, Image } from "react-grid-gallery";
-import { ThumbnailImageProps } from "react-grid-gallery";
-import NImage from "next/image";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
- export default function SponsorGallery() {
-    const images = [
-        {
-            src: "/sponsors/chrishillman.jpg",
-            alt: "Chris Hillman",
-        },
-        {
-            src: "/sponsors/abc.jpg",
-            alt: "Abergavenny Brake & Clutch",
-            href: "http://www.abcmotorfactors.com/",
-        },
-        {
-            src: "/sponsors/cwbs.jpg",
-            alt: "Caldicot Windows & Building Services",
-            href: "mailto:chippyatwork78@gmail.com"
-        },
-        {
-            src: "/sponsors/foundry4x4.jpg",
-            alt: "Foundry 4x4",
-            href: "https://foundry4x4.co.uk/"
-        },
-        {
-            src: "/sponsors/muddyseries4x4.jpg",
-            alt: "Muddy Series 4x4",
-            href: "https://www.ebay.co.uk/str/seriesuser4x4"
-        },
-        {
-            src: "/sponsors/whitecliff4x4.jpg",
-            alt: "Whitecliff 4x4",
-            href: "https://www.whitecliff4x4.co.uk/"
+type Sponsor = {
+    src: string
+    alt: string
+    href: string | null
+}
+
+export default function SponsorGallery() {
+
+    const [images, setImages] = useState<Sponsor[]>([])
+
+    useEffect(() => {
+        let images: Sponsor[] = [
+            {
+                src: "/sponsors/chrishillman.jpg",
+                alt: "Chris Hillman",
+                href: null,
+            },
+            {
+                src: "/sponsors/abc.jpg",
+                alt: "Abergavenny Brake & Clutch",
+                href: "http://www.abcmotorfactors.com/",
+            },
+            {
+                src: "/sponsors/cwbs.jpg",
+                alt: "Caldicot Windows & Building Services",
+                href: "mailto:chippyatwork78@gmail.com",
+            },
+            {
+                src: "/sponsors/foundry4x4.jpg",
+                alt: "Foundry 4x4",
+                href: "https://foundry4x4.co.uk/",
+            },
+            {
+                src: "/sponsors/muddyseries4x4.jpg",
+                alt: "Muddy Series 4x4",
+                href: "https://www.ebay.co.uk/str/seriesuser4x4",
+            },
+            {
+                src: "/sponsors/whitecliff4x4.jpg",
+                alt: "Whitecliff 4x4",
+                href: "https://www.whitecliff4x4.co.uk/",
+            }
+        ]
+
+        images = images.sort(() => Math.random() - 0.5)
+        setImages(images)
+    }, [])
+
+    if (images.length == 0) {
+        return <main><div className="h-dvh">Loading...</div></main>
+    }
+
+    const img = (image: Sponsor) => {
+        let result = <Image src={image.src} width={0} height={0} className="w-full" alt={image.alt} fill={false} />
+
+        if (image.href !== null) {
+            result = <a href={image.href} target="_blank">{result}</a>
         }
-    ]
 
-    images.sort(() => Math.random() - 0.5)
+        return result
+    }
 
     return (
-        <main>TODO</main>
+        <main>
+            <div className="grid grid-cols-1 md:grid-cols-2">
+            {images.map((image, id) => (
+                <div key={id} className="col-span-1">
+                    <h2 className="font-bold">{image.alt}</h2>
+                    {img(image)}
+                </div>
+            ))}
+            </div>
+        </main>
     )
 }
