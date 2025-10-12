5469631946da5cb06c6e95cc7c17bc8e5aa7bdda
Archey Barrell <a@archbar.me>
2025-10-12 17:28:21 +0100

    add fb group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import SpannerBlog from "@/components/blog";
 import { getPosts } from "@/spanner/blog";
+import { wwlrcFacebookGroupLink } from "@/spanner/wwlrc";
+import { FacebookIcon } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Wye & Welsh LRC | Home",
@@ -16,6 +18,22 @@ export default async function Home() {
 
       <p className="mb-2">
         Welcome to the Wye & Welsh Land Rover Club website!
+      </p>
+      <p className="mb-2">
+        Take a look around our website to find out more about the club, see what
+        events are coming up, and how you can get involved involved; we’ve got a
+        Facebook group too!
+      </p>
+
+      <p className="mb-4">
+        <a
+          href={wwlrcFacebookGroupLink}
+          target="_blank"
+          className="inline-flex items-center w-128 px-2 py-2 pr-4 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors font-medium"
+        >
+          <FacebookIcon className="mr-2" />
+          Join our Facebook group
+        </a>
       </p>
 
       <h2 className="text-xl font-bold mt-2">Latest News</h2>
