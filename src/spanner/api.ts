b1e626985f1a486d9a9e7344d4dabd1aa6492c74
Archey Barrell <a@archbar.me>
2025-12-19 00:59:14 +0000

    wrap instead

--- src/spanner/api.ts
+++ src/spanner/api.ts
@@ -1,14 +1,36 @@
-import { base, ExtractRouteParams, Query } from "pathcat";
+import { base, ExtractRouteParams, Query, pathcat } from "pathcat";
 
 export const spannerPathCat = base("https://spanner.wwlrc.co.uk");
 
-export async function spannerApiFetch<Path extends string>(
+function wrap<R>(
+  fn: (path: string) => R,
+): <Path extends string>(
+  base: string,
   path: Path,
   ...query: [ExtractRouteParams<Path>] extends [never]
     ? [query?: Query<Path>]
     : [query: Query<Path>]
-) {
-  let response = await fetch(spannerPathCat(path, ...query));
-  // TODO: probably worth putting some more fancy error handling here
-  return response.json();
+) => R {
+  return (base, path, ...query): R => fn(pathcat(base, path, ...query));
 }
+
+function wrapWithBase<R>(
+  base: string,
+  fn: (path: string) => R,
+): <Path extends string>(
+  path: Path,
+  ...query: [ExtractRouteParams<Path>] extends [never]
+    ? [query?: Query<Path>]
+    : [query: Query<Path>]
+) => R {
+  return (path, ...query): R => wrap(fn)(base, path, ...query);
+}
+
+export const spannerApiFetch = wrapWithBase<Promise<any>>(
+  "https://spanner.wwlrc.co.uk",
+  async (path) => {
+    let response = await fetch(path);
+    // TODO: probably worth putting some more fancy error handling here
+    return response.json();
+  },
+);
